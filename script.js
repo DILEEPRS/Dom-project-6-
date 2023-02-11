0871396ec395c0eaadf89fd0f12226d9a46cb622
DILEEP R S <deepurs666@gmail.com>
2023-02-11 13:12:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// project 6 Output
+
+// first output
+
+// let tg = document.querySelector('header img');
+// tg.remove();
+
+
+
+
+
+// let img1 = document.createElement('img');
+// img1.src = "./assets/ineuron-logo.png";
+
+// let target = document.querySelector('header');
+
+// target.appendChild(img1);
+
+// second output
+
+let tg = document.querySelector('header img');
+
+tg.remove();
+
+let tg1 = document.querySelector('.top_img img');
+
+let tg3 = document.querySelector('.hero');
+
+tg1.remove();
+
+tg3.remove();
